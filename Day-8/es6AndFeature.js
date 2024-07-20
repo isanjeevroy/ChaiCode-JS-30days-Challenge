e18fd93ecdbaed152210105efa8da8761fdb7aa2
Sanjeev Roy <sanjeev15062002@gmail.com>
2024-07-20 23:50:16 +0530

    Day-8

---
+++ Day-8/es6AndFeature.js
@@ -0,0 +1,26 @@
+//Activity 1: Template Literals
+//Task 1
+let name = "Sanjeev"
+let age = 21
+console.log(`${name} is ${age} years old`);
+
+//Task 2
+console.log(`Hey, This is Sanjeev,
+    how are you guys,
+    sab thik thak`);
+
+//Activity 2: Destructuring
+//Task 3
+const arr =[1,2,3,4,5];
+const[i,j] = arr;
+console.log(i);
+console.log(j);
+
+//Task 4
+const book = {
+    title:"Half Girlfriend",
+    author:"Chetan Bhagat"
+}
+const {title, author} = book;
+console.log(title);
+console.log(author);
