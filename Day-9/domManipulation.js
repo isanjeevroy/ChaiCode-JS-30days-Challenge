aa82b0e0cb31ae15e7ccc1dc364194fe998763e2
Sanjeev Roy <sanjeev15062002@gmail.com>
2024-07-22 23:58:25 +0530

    day 10

--- Day-9/domManipulation.js
+++ Day-9/domManipulation.js
@@ -1,9 +1,24 @@
 //Activity 1: Selecting and Manipulating elements
+//Task1
+
+//Task2
 
 //Activity 2: Creating and appending elements
+//Task3
+
+//Task4
 
 //Activity 3: Removing elements
+//Task5
+
+//Task6
 
 //Activity 4: Modifying attributes and classes
+//Task7
+
+//Task8
 
 //Activity 5: Event handling
+//Task9
+
+//Task10
