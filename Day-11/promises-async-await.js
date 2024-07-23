be545d09ef2c5c12e40ea0f7146f4486f99bdbbd
Sanjeev Roy <sanjeev15062002@gmail.com>
2024-07-23 23:38:37 +0530

    day 11

--- Day-11/promises-async-await.js
+++ Day-11/promises-async-await.js
@@ -14,4 +14,18 @@ function delayedMessage() {
     console.log(message);
   })
 
+  //Task 2
+  function delayedError() {
+    return new Promise((_, reject) => {
+      setTimeout(() => {
+        reject(new Error("An error occurred after 2 seconds!"));
+      }, 2000);
+    });
+  }
+  
+  delayedError()
+    .catch((error) => {
+      console.error(error.message);
+    });
+  
   
